7f284c306ca909c5689c2405f3d5ce9c0a6be510
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-08-27 10:27:19 -0300

    refactor: página de faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import Link from "../src/components/Link";
+import FAQScreen from "../src/screens/FaqScreen";
 
 export async function getStaticProps() {
     const URL_API = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
@@ -14,21 +13,4 @@ export async function getStaticProps() {
     } 
 }
 
-export default function FaqPage({faq}){
-    return (
-        <div>
-             <Head>
-                <title>FAQ - Alura Cases</title>
-            </Head>
-            <h1>Alura Cases - Página de FAQ</h1>
-            <Link href='/'>
-                Ir para a página Home
-            </Link>
-            {faq.map(({question, answer}) => (
-                <article key={question}>
-                    <li>{question}</li>
-                </article>
-            ))}
-        </div>
-    );
-}
+export default FAQScreen;
